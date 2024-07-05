9e366fb3bb69727b411e395114c6eeb72425f77b
MonishJasayah <monish@kidiyo.com>
2024-07-05 21:08:02 +0530

    Updated And Player Mouse Look To Actions

--- Assets/Scripts/Player/Player_Actions.cs
+++ Assets/Scripts/Player/Player_Actions.cs
@@ -1,132 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using GameEnum;
 using GameEnum.Templates;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Player_Actions : MonoBehaviour
 {
-    [Header("Variables")] 
-    [SerializeField] private float _maxRayDistance = 5f;
-        
-    Camera _mainCamera;
-    Ray _ray = new Ray();
-    Ray _ray2 = new Ray();
-    RaycastHit _hit;
+    #region Variables
+
+    [Header("Main Camera")]
+    [SerializeField] private MainCamera mainCamera;
     
-    private Door door = new Door();
-    private Lights_Environment lights = new Lights_Environment();
     private Player_Audio _player_Audio;
     private GameObject _candle;
-    
-    
+    private Lights_Environment lights = new Lights_Environment();
+
+
+    #endregion
+
+
+    #region Unity Methods
     private void Start()
     {
-        _mainCamera = Camera.main;
         _player_Audio = this.GetComponent<Player_Audio>();
+        
     }
     private void Update()
+    {
+        CheckForPlayerInputs();
+    }
+    #endregion
+  
+
+    private void CheckForPlayerInputs()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             CheckForInteractables();
         }
+
         if (Input.GetKeyDown(KeyCode.I))
         {
             CheckForInventory();
         }
-        
     }
-    private void FixedUpdate()
-    {
-        Check_UI();
-    }
-    private void Check_UI()
-    {
-        _ray2 = _mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(_ray2, out _hit, _maxRayDistance))
-        {
-           if (_hit.collider.gameObject.CompareTag("Door")||_hit.collider.gameObject.CompareTag("Object")||_hit.collider.gameObject.CompareTag("Puzzle")
-               ||_hit.collider.gameObject.CompareTag("Camera1"))
-           {
-                MainCanvas_UI.Instance.Show_Helper();
-           }
-           else
-           {
-               MainCanvas_UI.Instance.Hide_Helper();
-               
-           }
-        }
-        else
-        {
-            MainCanvas_UI.Instance.Hide_Helper();
-        }
-        
-    }
-
     private void CheckForInventory()
     {
         InventoryManager.Instance.EnableInventory();
     }
-
-    private void CheckForJournal()
-    {
-        //MainCanvas_UI.Instance.Show_LetterPanel();
-    }
-
     private void CheckForInteractables()
     {
-        _ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-        // Define the maximum raycast distance.
-        // Perform the raycast.
-        if (Physics.Raycast(_ray, out _hit, _maxRayDistance))
+        IInteractable interactableItem=null;
+        if (this.mainCamera.IsInteractableObjectInRange(out interactableItem))
         {
-            if (_hit.collider.TryGetComponent(out IInteractable interactable))
+            if (interactableItem != null)
             {
-                interactable.Interact();
-            }         
+                interactableItem.Interact();
+            }
         }
     }
-    private void PickCandle()
-    {
-        _candle.SetActive(true);
-    }
-    private void PickLetter(GameObject go)
-    {
-        _player_Audio.Player_Audio_PickUp_Letter();
-        //go.GetComponent<Letter>().SelectedTheLetter();
-    }
-    private void ToggleDoor(GameObject go)
-    {
-        door = go.GetComponent<Door>();
-        if (door.isUnlocked)
-        {
-            door.ToggleDoor();
-        }
-        else
-        {
-            if (door._doorType == keyType.None)
-            {
-                door.UnlockDoor();
-                door.ToggleDoor();
-            }
-            if (InventoryManager.Instance.CheckKeyAvailable(door._doorType))
-            {
-                door.UnlockDoor();
-                door.ToggleDoor();
-            }
-            else
-            {
-                MainCanvas_UI.Instance.Show_Message("LOCKED");
-            }
-            
-        }
-      
-       
-    }
-    
     private void ToggleLights(GameObject go)
     {
         _player_Audio.Player_Audio_PicKUp_Object();
@@ -136,5 +72,4 @@ public class Player_Actions : MonoBehaviour
         else { lights.SetLightActive(true); }
     }
     
-
 }
